fa4ee1b83fdae5369a06a898cccc162ba8050b23
Miles Cramer <02miles.cramer@gulllakecs.org>
2020-03-03 14:03:48 -0500

    Initial TelloTrainingLabs Commit

--- TelloTrainingLabs.py
+++ TelloTrainingLabs.py
@@ -4,37 +4,33 @@
 
 import threading, socket, sys, time, subprocess
 
+
+# GLOBAL VARIABLES DECLARED HERE....
 host = ''
 port = 9000
 locaddr = (host,port)
-rad = 100
 tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
 
-# Create a UDP socketd
+
+
+# Creates a UDP socketd
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 sock.bind(locaddr)
 
+
 def recv():
-    # Message receiver
     count = 0
-    global wait
-    global breakr
     while True:
         try:
-            data, server = sock.recvfrom(1518) # Get data from UDP port 1518?
-            print(data.decode(encoding="utf-8")) # Encode data so drone can read it
-            if data.decode(encoding="utf-8") == "ok": # Ok is good
-                wait = False
-            if breakr == True: # Stop the function
-                break
+            data, server = sock.recvfrom(1518)
+            print(data.decode(encoding="utf-8"))
         except Exception:
-            print ('\nError recv\n')
+            print ('\n****Keep Eye on Drone****\n')
             break
 
-def sendmsg(msg, sleep = 5):
-    # Message sender
 
+def sendmsg(msg, sleep = 6):
     print("Sending: " + msg)
     msg = msg.encode(encoding="utf-8")
     sock.sendto(msg, tello_address)
@@ -43,35 +39,37 @@ def sendmsg(msg, sleep = 5):
 # recvThread create
 recvThread = threading.Thread(target=recv)
 recvThread.start()
-print("\nMiles Cramer")
-print("Program Name: Square")
-print("Date: 02.25.20")
-print("\n****CHECK YOUR TELLO WIFI ADDRESS")
+
+
+# CREATE FUNCTIONS HERE....
+
+
+print("\nFirst & Last Names")
+print("Program Name: ")
+print("Date: ")
+print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
-print(input("\nAre you ready to take flight? "))
-print("\nStarting Drone!\n")
-
-#Create functions here
-
-def square():
-    for i in range(4):
-        sendmsg('forward 100')
-        sendmsg('cw 90')
+ready = input('\nAre you ready to take flight: ')
 
 
-
-
-time.sleep(1)
 try:
-    sendmsg('command', 0)
-    sendmsg('takeoff')
+    if ready.lower() == 'yes':
+        print("\nStarting Drone!\n")
 
-    square()
-    # Commands go here, read the SDK for commands.
+        sendmsg('command', 0)
+        sendmsg('takeoff')
 
-    sendmsg('land')
-    print('Great Flight!!!')
-except KeyboardInterrupt: # Ctrl+C (Use if drone is going crazy and to exit the application)
+        # Review the (SDK) Software Development Kit resource for Drone Commands
+        # Delete these comments before writing your program
+
+        sendmsg('land')
+
+        print('\nGreat Flight!!!')
+
+    else:
+        print('\nMake sure you check WIFI, surroundings, co-pilot is ready, re-run program\n')
+except KeyboardInterrupt:
     sendmsg('emergency')
+
 breakr = True
 sock.close()
